1f3415c0a1a5db2576f27db1a45617417784cad5
banyanlei <1428609906@qq.com>
2022-09-26 22:21:06 +0800

    initialize; add MonotonousStack

---
+++ AlgorithmDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonotonousStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in GetMaxRightArray(new int[] { 2, 6, 3, 8, 10, 9 }))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static IEnumerable<int> GetMaxRightArray(int[] array)
+        {
+            if (array == null || array.Length == 0) return array;
+            Stack<int> stack = new Stack<int>();
+            int[] res = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                while (stack.Count != 0 && array[stack.Peek()] < array[i])
+                {
+                    int index = stack.Pop();
+                    res[index] = array[i];
+                }
+                stack.Push(i);
+            }
+            while(stack.Count != 0)
+            {
+                res[stack.Pop()] = -1;
+            }
+
+            return res;
+        }
+    }
+}
